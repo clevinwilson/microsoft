8ca5089f9c8fb634645712d8dd7edad34a9e554c
Clevin M.Wilson <clevinmwilson@gmail.com>
2022-11-08 17:40:23 +0530

    nav menu icon change when close feature added

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function openNav() {
+    let openbtn = document.getElementById("open-btn");
+    let closebtn = document.getElementById("close-btn");
+    openbtn.style.display = "none"
+    closebtn.style.display = "block";
+}
+
+function closeNav() {
+    let openbtn = document.getElementById("open-btn");
+    let closebtn = document.getElementById("close-btn");
+    closebtn.style.display = "none";
+    openbtn.style.display = "block"
+}
